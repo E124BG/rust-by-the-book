08eb69557d455f63d81446b2473e11fe745a1bd3
E124BG <graak@live.fr>
2023-02-09 09:34:01 +0100

    conversion string => u32 and pattern matching implemented

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 //documentation of crates accessed with $cargo doc --open
 
 fn main() {
@@ -19,5 +20,17 @@ fn main() {
     //& means that here its a reference to the mutable var "guess"
     // references can be used without having to copy the entire var in memory
 
+    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+    //trim removes any whithespace, parse converts string to number (could fail, hence the except)
+    //the reuse of "guess" is a shadowing, often used to convert one datatype to another
+
     println!("You guessed {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+
 }
