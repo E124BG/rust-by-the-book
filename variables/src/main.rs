40fac02daa58c4c0c8c38f768fe0dbd6b3a3ca7f
E124BG <graak@live.fr>
2023-02-10 09:11:01 +0100

    constants example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,28 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is {x}");
-    // x = 6;
-    //Impossile to assign twice an immutable var
-    // println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is {x}");
+
+    //constants
+
+    //constants are immutable, declared in FULL_CAPS_WITH_UNDERSCORES
+    //their type must be annotated
+    //they can be declared in any scope, including global scope
+    //have to be set to a constant expression (not result of a computation done at runtime)
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    //shadowing
+
+    
 }
+
+// fn error1() {
+//  Impossile to assign twice an immutable var
+//     let x = 5;
+//     println!("The value of x is {x}");
+//     x = 6;
+//     println!("The value of x is {x}");
+// }
+
