43179dc444183be69544321953d2dddafb9f9143
E124BG <graak@live.fr>
2023-02-11 16:50:52 +0100

    another function created

---
+++ chapter_3_programming_concepts/functions/target/debug/deps/functions-4704d0fb65c9ab4c.d
@@ -0,0 +1,5 @@
+/home/eliott/Desktop/rust-by-the-book/chapter_3_programming_concepts/functions/target/debug/deps/functions-4704d0fb65c9ab4c: src/main.rs
+
+/home/eliott/Desktop/rust-by-the-book/chapter_3_programming_concepts/functions/target/debug/deps/functions-4704d0fb65c9ab4c.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_3_programming_concepts/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/eliott/Desktop/rust-by-the-book/chapter_3_programming_concepts/functions/target/debug/functions: /home/eliott/Desktop/rust-by-the-book/chapter_3_programming_concepts/functions/src/main.rs
