2b3cc98df6acbdad4c4669376a575e2195bda438
E124BG <graak@live.fr>
2023-02-11 10:30:10 +0100

    numeric operations examples

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -23,4 +23,40 @@ fn main() {
     let big_number : u32 = 100_000;
 
     println!("A big number : {big_number}");
+
+    //Floating point numbers type
+
+    //floating points are signed, default if f64
+    //represented as IEEE-754 standard
+    //f64 is double precision
+    //f32 is single precision
+
+    let x = 2.03;
+
+    let y : f32 = 2.0;
+
+    //numeric operations
+
+    // addition
+    let sum = 5 + 10;
+
+    println!("This is 5 + 10: {sum}");
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("This is 95.5 - 4.3: {difference}");
+
+    // multiplication
+    let product = 4 * 30;
+    println!("This is 4 * 30: {product}");
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("This is 56.7 / 32.2: {quotient}");
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("This is 43 % 5: {remainder}");
+
+
 }
