08fae70c7ba91cebddfcd30b701c5a899ac825a1
E124BG <graak@live.fr>
2023-02-13 18:13:27 +0100

    Used clippy

--- chapter_2_guessing_game/target/debug/deps/guessing_game-4c371d7934e5d59e.d
+++
@@ -1,5 +0,0 @@
-/home/eliott/Desktop/rust-by-the-book/chapter_2_guessing_game/target/debug/deps/guessing_game-4c371d7934e5d59e.rmeta: src/main.rs
-
-/home/eliott/Desktop/rust-by-the-book/chapter_2_guessing_game/target/debug/deps/guessing_game-4c371d7934e5d59e.d: src/main.rs
-
-src/main.rs:
--- chapter_2_guessing_game/target/debug/deps/guessing_game-c8bbb1b019307547.d
+++
@@ -1,8 +0,0 @@
-/home/eliott/Desktop/rust-by-the-book/chapter_2_guessing_game/target/debug/deps/guessing_game-c8bbb1b019307547.rmeta: src/main.rs Cargo.toml
-
-/home/eliott/Desktop/rust-by-the-book/chapter_2_guessing_game/target/debug/deps/guessing_game-c8bbb1b019307547.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
