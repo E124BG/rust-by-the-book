50eb15f4813d6e9d1a48255a25583efa2ab6b543
E124BG <graak@live.fr>
2023-02-13 17:52:58 +0100

    used rustfmt to format the code according to style guidelines

--- chapter_2_guessing_game/src/main.rs
+++ chapter_2_guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 //documentation of crates accessed with $cargo doc --open
 
 fn main() {
@@ -18,14 +18,15 @@ fn main() {
         let mut guess = String::new();
         // mut means that the var "guess" is mutable
 
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
         //& means that here its a reference to the mutable var "guess"
         // references can be used without having to copy the entire var in memory
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
-
         };
         //trim removes any whithespace, parse converts string to number
         //if parse fails because guess is not a number, then try again
