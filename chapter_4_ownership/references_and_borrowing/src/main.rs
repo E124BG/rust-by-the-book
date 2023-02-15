02b38479bbd40ae46f36984354379612a803ad77
E124BG <graak@live.fr>
2023-02-15 09:45:42 +0100

    example of 2 mutable refs at the same time

--- chapter_4_ownership/references_and_borrowing/src/main.rs
+++ chapter_4_ownership/references_and_borrowing/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    //how to use calculate_length without giving ownership of the value
+    //we use a reference instead of a pointer
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    //references have 2 major constraints to avoid race conditions
+
+    //1.We can mutate only one reference at a time
+    //2.We can't borrow a reference that is being mutated
+
+    //     let mut s = String::from("hello");
+
+    //     let r1 = &mut s;
+    //     let r2 = &mut s;
+
+    //     println!("{}, {}", r1, r2);
+    //      won't compute because 2 mutable refs at the same time
 }
+
+fn calculate_length(s: &String) -> usize {
+    //reference &String refers to the value without taking ownership
+    s.len()
+} //s is not dropped (its owner is still in scope)
