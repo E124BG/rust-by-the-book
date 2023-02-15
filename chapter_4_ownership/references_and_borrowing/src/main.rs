2e621be4eb4aef62361d25165fd68554d847345f
E124BG <graak@live.fr>
2023-02-15 09:52:52 +0100

    finished comments on references and borrowing

--- chapter_4_ownership/references_and_borrowing/src/main.rs
+++ chapter_4_ownership/references_and_borrowing/src/main.rs
@@ -18,10 +18,27 @@ fn main() {
     //     let r2 = &mut s;
 
     //     println!("{}, {}", r1, r2);
-    //      won't compute because 2 mutable refs at the same time
+    //      won't compile because 2 mutable refs at the same time
+
+    // let mut s = String::from("hello");
+
+    // let r1 = &s; // no problem
+    // let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+
+    // println!("{}, {}, and {}", r1, r2, r3);
+
+    //won't compile because mutable is being borrowed
+
+
+
 }
 
 fn calculate_length(s: &String) -> usize {
     //reference &String refers to the value without taking ownership
     s.len()
 } //s is not dropped (its owner is still in scope)
+
+//Recap :
+//At any given time, we can have either one mutable reference or any number of immutable references
+//References must always be valid
