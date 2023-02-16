09381d41bad4a4a74601013accf83fc82c75f02a
E124BG <graak@live.fr>
2023-02-16 08:42:58 +0100

    wrote function without String slice

--- chapter_4_ownership/slice/src/main.rs
+++ chapter_4_ownership/slice/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+
+    let hello: String = String::from("Hello world!");
+
+    println!("{}",hello);
+
+    //function without string slice
+
+    first_word(&hello);
+
+
+}
+
+fn first_word(s: &String) -> usize {//Takes a String ref in, gives an index back
+    let bytes = s.as_bytes();//let's convert the string into bytes
+
+    println!("{:?}",bytes);
+
+    for (i, &item) in bytes.iter().enumerate() {//let's iterate over the array obtained
+        if item == b' ' {//we search for the byte literal syntax of ' '
+            return i;//return current index if we find a space
+        }
+    }
+
+    s.len()//return length of the String if it contains no space (whole String is one word)
 }
