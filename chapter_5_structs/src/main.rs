01570325b11976014bb8ce2b7a2e1c6ce68059e7
E124BG <graak@live.fr>
2023-02-17 21:00:47 +0100

    Chapter 5 : structs

---
+++ chapter_5_structs/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    //structs are used to hold multiple related values
+    //they're similar to object's data attributes in object-oriented languages
+
+
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
