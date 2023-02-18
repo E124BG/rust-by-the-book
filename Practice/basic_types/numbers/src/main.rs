e1ca05544bb67643edab8d4e1ff1656eefc67a7c
E124BG <graak@live.fr>
2023-02-18 19:30:18 +0100

    exercise 3

--- Practice/basic_types/numbers/src/main.rs
+++ Practice/basic_types/numbers/src/main.rs
@@ -2,14 +2,16 @@ fn main() {
     exercise_1();
 
     exercise_2();
+
+    exercise_3();
 }
 
 fn exercise_1() {
     // Remove something to make it work
     let x: i32 = 5;
-    let mut y = 5;
+    let mut _y = 5;
 
-    y = x;
+    _y = x;
 
     let _z = 10; // Type of z ?
 
@@ -18,9 +20,20 @@ fn exercise_1() {
 
 fn exercise_2() {
     // Fill the blank
-    let v: u16 = 38_u8 as u16;
+    let v: u16 = 32_u8 as u16;
 
     println!("{}", v);
 
     println!("Success!");
+}
+
+fn exercise_3() {
+    let x: u32 = 5;
+    assert_eq!("u32".to_string(), type_of(&x));
+
+    println!("Success!");
+}
+
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
 }
