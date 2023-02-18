880418a0fb39fbe066c6a969b59dbd0537c68f5d
E124BG <graak@live.fr>
2023-02-18 19:23:59 +0100

    exercse 2

--- Practice/basic_types/numbers/src/main.rs
+++ Practice/basic_types/numbers/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     exercise_1();
+
+    exercise_2();
 }
 
 fn exercise_1() {
@@ -8,8 +10,17 @@ fn exercise_1() {
     let mut y = 5;
 
     y = x;
-    
-    let _z = 10; // Type of z ? 
+
+    let _z = 10; // Type of z ?
+
+    println!("Success!");
+}
+
+fn exercise_2() {
+    // Fill the blank
+    let v: u16 = 38_u8 as u16;
+
+    println!("{}", v);
 
     println!("Success!");
 }
