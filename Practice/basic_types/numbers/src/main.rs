4d94bee5a4ab076ca807a802e3364d8760ca7dc2
E124BG <graak@live.fr>
2023-02-18 19:17:41 +0100

    exercise 1

---
+++ Practice/basic_types/numbers/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    exercise_1();
+}
+
+fn exercise_1() {
+    // Remove something to make it work
+    let x: i32 = 5;
+    let mut y = 5;
+
+    y = x;
+    
+    let _z = 10; // Type of z ? 
+
+    println!("Success!");
+}
